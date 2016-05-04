ee82648e072c250493d64982c5163609441327d7
Vishruth <harithsa@outlook.com>
2016-05-04 23:00:56 +0530

    Mail Settings

--- contact/contact.php
+++ contact/contact.php
@@ -8,7 +8,7 @@
 	$message = "Name: ".$name." || Email: ".$email." || Subject: ".$subject." || Message: ".$message;
 	
 	/* Replace YOUR_MAIL With Your Mail Address inside '' */
-	if ( mail( 'YOUR_MAIL', $mail_subject, $message, "From:" . $email ) ) {
+	if ( mail( 'info@aricctumkur.com', $mail_subject, $message, "From:" . $email ) ) {
 		echo "Thank you <strong>$name</strong> for contacting with us !!!";
 	} else {
 		echo "Error in sending message !!! Please try again";
