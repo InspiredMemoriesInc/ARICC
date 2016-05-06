57ebcda4f43adc4845ee54c3e13b80e9129f20ba
Vishruth <harithsa@outlook.com>
2016-05-07 00:25:40 +0530

    Mail Changes

--- contact/contact.php
+++ contact/contact.php
@@ -4,11 +4,11 @@
 	$email = $_POST[ 'contact-email' ];
 	$subject = $_POST[ 'contact-subject' ];
 	$message = $_POST[ 'contact-message' ];
-	$mail_subject = $subject . "( Customer Contact )";
+	$mail_subject = $subject . "(Visitor Contact)";
 	$message = "Name: ".$name." || Email: ".$email." || Subject: ".$subject." || Message: ".$message;
 	
 	/* Replace YOUR_MAIL With Your Mail Address inside '' */
-	if ( mail( 'info@aricctumkur.com', $mail_subject, $message, "From:" . $email ) ) {
+	if ( mail( 'info@aricctumkur.org', $mail_subject, $message, "From:" . $email ) ) {
 		echo "Thank you <strong>$name</strong> for contacting with us !!!";
 	} else {
 		echo "Error in sending message !!! Please try again";
